96fc0afd39bc1bdad66b52755aa6c3a089dfdf6b
azhamkochyan <aram.zhamkochyan@dataart.com>
2022-06-21 00:41:15 +0200

    main page's first touches finished

--- app.js
+++ app.js
@@ -8,12 +8,30 @@ function pageTransition(){
     for (let i = 0; i < secBtn.length; i++){
         secBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
-            //currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
             currentBtn[0].classList.remove('active-btn'); 
-            //this.className += ' active-btn';
             this.classList.add('active-btn');
         });
     }
+    
+    // Sections active class
+    allSections.addEventListener('click', e => {
+        const id = e.target.dataset.id;
+        if(id){
+            // remove selected from the other btns
+            secBtns.forEach(btn => {
+                btn.classList.remove('active');
+            });
+            e.target.classList.add('active');
+            
+            // hide other sections
+            sections.forEach(section => {
+               section.classList.remove('active'); 
+            });
+            
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    });
 }
 
 pageTransition();
